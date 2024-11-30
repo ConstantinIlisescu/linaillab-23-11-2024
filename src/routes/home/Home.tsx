1e85038e1902324aacce27daf2176453ca69f189
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-11-30 17:36:25 +0000

    welcome section  updates

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -59,26 +59,28 @@ const Home = () => {
         </div>
       </div>
       <div className="pt-28 w-full flex ">
-        <div className="welcome-overlay"></div>
-        <div className="max-w-lg md:pl-16">
-          <div className="text-4xl pb-10">
-            <p className="pb-2 aclonica-regular gradient-text">
-              WELCOME TO
-              <br />
-              L.I Nail Lab
+        <div className="welcome-overlay hidden"></div>
+        <Container>
+          <div className="max-w-lg">
+            <div className="text-4xl pb-10">
+              <p className="pb-2 aclonica-regular gradient-text">
+                WELCOME TO
+                <br />
+                L.I Nail Lab
+              </p>
+            </div>
+            <p className=" pr-10 md:text-lg text-balance">
+              L.I Nail Lab is a home-based nail salon located in the Hednesford
+              neighbourhood, offering a unique and personalized experience that
+              combines style and exceptional service. My cosy and inviting salon
+              features top-of-the-line products, exclusive polish brands and
+              colours, a range of free soft drinks and homemade coffee. My
+              commitment to quality ensures that every visit to my salon feels
+              like a luxurious and indulgent escape in the comfort of your own
+              neighbourhood.
             </p>
           </div>
-          <p className=" pr-10 md:text-lg text-balance">
-            L.I Nail Lab is a home-based nail salon located in the Hednesford
-            neighbourhood, offering a unique and personalized experience that
-            combines style and exceptional service. My cosy and inviting salon
-            features top-of-the-line products, exclusive polish brands and
-            colours, a range of free soft drinks and homemade coffee. My
-            commitment to quality ensures that every visit to my salon feels
-            like a luxurious and indulgent escape in the comfort of your own
-            neighbourhood.
-          </p>
-        </div>
+        </Container>
       </div>
       <div className=" pt-28 mx-auto w-full max-w-6xl">
         <Container>
