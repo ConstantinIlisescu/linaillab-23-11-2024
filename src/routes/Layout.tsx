b2c0284f8aa3bf364b9f92a84422ae85f3b9367e
Costa Ilisescu <coste_ilisescu@yahoo.com>
2024-12-21 15:54:51 +0000

    Change folder structure

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Navigation from "@/routes/navigation/Navigation";
-import Footer from "@/routes/footer/Footer";
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
 import useScrollToEffect from "@/utils/hooks/useScrollToEffect";
 
 const Layout = () => {
