984c6466638f438dcff4f3def36e5c29fd1f9708
Costa Ilisescu <coste_ilisescu@yahoo.com>
2025-01-02 20:52:27 +0000

    updates

--- src/data/servicesPage.ts
+++ src/data/servicesPage.ts
@@ -7,7 +7,24 @@ export const SERVICES_LIST = [
       "Manicure is a pampering hand treatment that involves trimming, shaping, and filing nails, as well as neatening the cuticles. The final touch involves elegantly painting your nails with a colour of your preference.",
     services: [
       {
-        price: "£30",
+        price: "£40",
+        serviceName: "Gel Nail Extension size 1-2",
+        serviceDescription:
+          "Gel nail extensions are a popular method of achieving longer and stronger nails. The process entails building hard gel on a natural nail and curing it with LED light. A nail form is utilized to extend the length of the nails. The first step in the application process involves preparing the nails by filing, cleaning cuticles, and buffing the nails. Subsequently, a nail form filled with top-quality gel is applied to the natural nail and cured under the LED light to create the desired length and finished with the chosen colours and design. Opting for this service will result in nails finished at a size between 1 and 2 (short to medium).",
+        duration: "approximately 3 hours",
+        additionalInfo: [
+          "If you are a new client or if your nails were done over four weeks ago, an additional £5 will be added to the total price.",
+        ],
+      },
+      {
+        price: "£50",
+        serviceName: "Gel Nail Extension  size 3-4",
+        serviceDescription:
+          "Gel nail extensions are a popular method for achieving longer and stronger nails. The process entails building hard gel on a natural nail and curing it with LED light. A nail form is utilized to extend the length of the nails. The first step in the application process involves preparing the nails by filing, cleaning cuticles, and buffing the nails. Subsequently, a nail form filled with top-quality gel is applied to the natural nail and cured under the LED light to create the desired length and finished with the chosen colours and design. Opting for this service will result in nails finished at a size between 3 and 4 (long to extra long).",
+        duration: "approximately 4 hours",
+      },
+      {
+        price: "£35",
         serviceName: "Gel Manicure Infill size 0-2",
         serviceDescription:
           "Gel Manicure Infill is a meticulous process that involves removing most of the old material or any lifted product. This is followed by the application of new material, shaping the nail to perfection, and adding colour or any chosen design. Opting for this service will result in nails finished at a size between 0 and 2 (short to medium size).",
@@ -26,28 +43,12 @@ export const SERVICES_LIST = [
           "If you are a new client or if your nails were done over four weeks ago, an additional £5 will be added to the total price.",
         ],
       },
-      {
-        price: "£40",
-        serviceName: "Gel Nail Extension size 1-2",
-        serviceDescription:
-          "Gel nail extensions are a popular method of achieving longer and stronger nails. The process entails building hard gel on a natural nail and curing it with LED light. A nail form is utilized to extend the length of the nails. The first step in the application process involves preparing the nails by filing, cleaning cuticles, and buffing the nails. Subsequently, a nail form filled with top-quality gel is applied to the natural nail and cured under the LED light to create the desired length and finished with the chosen colours and design. Opting for this service will result in nails finished at a size between 1 and 2 (short to medium).",
-        duration: "approximately 3 hours",
-        additionalInfo: [
-          "If you are a new client or if your nails were done over four weeks ago, an additional £5 will be added to the total price.",
-        ],
-      },
-      {
-        price: "£50",
-        serviceName: "Gel Nail Extension  size 3-4",
-        serviceDescription:
-          "Gel nail extensions are a popular method for achieving longer and stronger nails. The process entails building hard gel on a natural nail and curing it with LED light. A nail form is utilized to extend the length of the nails. The first step in the application process involves preparing the nails by filing, cleaning cuticles, and buffing the nails. Subsequently, a nail form filled with top-quality gel is applied to the natural nail and cured under the LED light to create the desired length and finished with the chosen colours and design. Opting for this service will result in nails finished at a size between 3 and 4 (long to extra long).",
-        duration: "approximately 4 hours",
-      },
+
       {
         price: "£30",
-        serviceName: "Polish Gel Manicure",
+        serviceName: "Biab Manicure",
         serviceDescription:
-          "Polish gel manicure is a service that uses a gel-based polish and requires a UV or LED light to cure the polish and lock it onto your nails. Gel polish is more durable than regular polish and typically stays chip-free for weeks. This involves the application of a gel material on top of the natural nail, shaping the nail to perfection, and adding colour or any chosen design.",
+          "Biab manicure is a service that uses a gel-based polish and requires a UV or LED light to cure the polish and lock it onto your nails. Gel polish is more durable than regular polish and typically stays chip-free for weeks. This involves the application of a gel material on top of the natural nail, shaping the nail to perfection, and adding colour or any chosen design.",
         duration: "approximately 2 hours 30 mins",
         additionalInfo: [
           "If you are a new client or if your nails were done over four weeks ago, an additional £5 will be added to the total price.",
